2e04072f5cddee259f5c9010971bc9aef06aa401
Andrey <Rikitikitime95@mail.ru>
2022-08-30 13:53:33 +0300

    save python new (change main.py)

--- main.py
+++ main.py
@@ -1,5 +1,42 @@
-age = 27
-print(age)
+# age = 27
+# print(age)
+# # целые числа - integer - int
+# res = 78 / 2
+# print(2 ** 3)  # возведение в степень **
+# print(res)
+# print(7 // 2)  # целочисленное деление
+# print(7 % 2)  # остаток от деления
+# sec = 1200
+# print(sec // 60)  # перевели в секунды
+# print(sec // 3600)  # перевели в часы
+# # [Ctrl] + [Alt] + [L] - форматирование в pycharm
+# # десятичное число - float
+# temp = 10
+# print(temp)
+# print(type(temp))  # напишет тип переменной
+# # строки - string - str
+# name = 'Andrey'
+# name2 = 'Ivan'
+# print(name, name2)
+# age = '50'  # сделали из int str
+# print(name + ' ' + name2)  # склеить строки с пробелом
+# print(name * 5)  # умножить строку 5 раз
+#
+# # input () - запросить у клиента, всегда превращает в строку
+# age = input('enter age: ')
+# print('Age:', age)
+# # [Ctrl] + [Shift] + [Z] - восстановить отмененное
+# # [Ctrl] + [Shift] + [/] — закомментировать/раскомментировать выделенные строки кода
+#
+# n1 = input('n1: ')
+# n2 = input('n2: ')
+# n1 = int(n1)  # инт преобразует str в int
+# n2 = float(n2)  # десятичное число
+# print('summ =', n1 + n2)
+print(6 >= 4)
+print(8 != 8)
 
-# целые числа - integer - int
-
+age = 19
+if age >= 18:
+    print('можете ехать')
+    print('удачного пути')
